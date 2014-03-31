9fd43305ea5d10538ad7202b2833660c1c7a9aa3
sofien <sofien@pc-sofien.esprit.tn>
2014-03-31 23:39:18 +0100

    Signed-off-by: sofien <sofien@pc-sofien.esprit.tn>

---
+++ tn.edu.esprit.gl8.tunisianWatch/ejbModule/tn/edu/esprit/gl8/tunisianWatch/impl/ClaimServicesLocal.java
@@ -0,0 +1,41 @@
+package tn.edu.esprit.gl8.tunisianWatch.impl;
+
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+
+import tn.edu.esprit.gl8.tunisianWatch.domain.Claim;
+
+@Local
+@Path("/Claim")
+public interface ClaimServicesLocal {
+
+	@GET
+	@Path("/addClaim/{location}/{priority}/{description}/{direction}/{statut}/{date}")
+	@javax.ws.rs.Produces(MediaType.APPLICATION_JSON)
+	public boolean addClaim(@PathParam(value = "location") String location,
+			@PathParam(value = "priority") String priority,
+			@PathParam(value = "description") String description,
+			@PathParam(value = "direction") String direction,
+			@PathParam(value = "statut") String statut,
+			@PathParam(value = "date") String date);
+
+	@GET
+	@Path("/listAllClaims")
+	@javax.ws.rs.Produces(MediaType.APPLICATION_JSON)
+	public List<Claim> getAllClaims();
+
+	@GET
+	@Path("/deleteClaim/{idclaim}")
+	@javax.ws.rs.Produces(MediaType.APPLICATION_JSON)
+	public boolean deleteClaim(int idclaim);
+
+	@GET
+	@Path("/findClaim/{location}")
+	@javax.ws.rs.Produces(MediaType.APPLICATION_JSON)
+	public List<Claim> getClaimsByMC(@PathParam(value = "motCle") String mC);
+}
---
+++ tn.edu.esprit.gl8.tunisianWatch/ejbModule/tn/edu/esprit/gl8/tunisianWatch/services/ClaimServices.java
@@ -0,0 +1,69 @@
+package tn.edu.esprit.gl8.tunisianWatch.services;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import tn.edu.esprit.gl8.tunisianWatch.domain.Claim;
+import tn.edu.esprit.gl8.tunisianWatch.impl.ClaimServicesLocal;
+
+@Stateless
+@LocalBean
+public class ClaimServices implements ClaimServicesLocal {
+
+	public ClaimServices() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	@Override
+	public boolean addClaim(String location, String priority,
+			String description, String direction, String statut, String date) {
+		boolean b = false;
+		try {
+			entityManager.persist(new Claim(description, location, direction,
+					statut, priority, date));
+			b = true;
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
+		return b;
+
+	}
+
+	@Override
+	public List<Claim> getAllClaims() {
+		Query query = entityManager.createQuery("select a from Claim a");
+		return query.getResultList();
+	}
+
+	@Override
+	public boolean deleteClaim(int idclaim) {
+		boolean b = false;
+		try {
+			Claim claim = entityManager.find(Claim.class, idclaim);
+			entityManager.remove(claim);
+			b = true;
+		} catch (Exception exception) {
+			exception.printStackTrace();
+
+		}
+
+		return b;
+	}
+
+	@Override
+	public List<Claim> getClaimsByMC(String mC) {
+		Query query = entityManager
+				.createQuery("select a from Claim a where a.location like :x");
+		query.setParameter("x", "%" + mC + "%");
+		return query.getResultList();
+	}
+}
